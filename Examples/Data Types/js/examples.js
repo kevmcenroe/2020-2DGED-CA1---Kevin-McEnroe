fbd64c7fd94b5acdd86eb9071daa4773560e04e3
niall mcguinness <niall.mcguinness@dkit.ie>
2020-10-02 12:24:13 +0100

    Added functions in objects demo

--- Examples/Data Types/js/examples.js
+++ Examples/Data Types/js/examples.js
@@ -102,15 +102,53 @@ p4.name = p1.name;
 
 
 var pickup1 = {
-    type: "ammo", //efficient? integer? TODO!!!
     value: 25,
     ttl: 60000, //time to live is 60,000ms = 60s
     model: {
+        name: "green opal",
+        type: "ammo", //efficient? integer? TODO!!!
         image: "pacman_spritesheet.jpg",
         type: "animated", //or "static"
-        upos: 50, //position within the spritesheet
-        vpos: 100
+        start_upos: 0, //position within the spritesheet
+        start_vpos: 100,
+        width: 50,
+        height: 100,
+        frames: 8
     }
+};
+
+for(var i = 0; i < pickup1.model.frames; i++){
+    console.log("drawing frame..."  + i);
+    console.log("wait until next frame");
 }
+
+var level1 = {
+    name: "the dreaded ice dungeon",
+    ttc: 600000, //10 mins max to complete
+    enemies: 10, //hobgoblins
+    lodif: 4, //level of difficulty
+    display: function(){
+        console.log(this.name + ",ttc: " + this.ttc);
+        //lots of code...
+    },
+    reset: function(){
+        this.ttc = 600000;
+        this.enemies = 10;
+    }
+};
+
+level1.display();
+level1.reset();
+
+var person1 = {
+    name: "niall",
+    height: 1.9,
+    eye: 1 //1=blue,2=green,3=brown,4=black
+};
+
+//dynamically added vars and functions are GREAT but UNPREDICTABLE
+person1.gender = "male";
+console.log(person1);
+delete person1.gender;
 
 
