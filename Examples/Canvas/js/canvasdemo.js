a2581aeaab4ac80e6324b02c049b9316090e9802
niall mcguinness <niall.mcguinness@dkit.ie>
2020-10-08 16:57:13 +0100

    Added arc demo

--- Examples/Canvas/js/canvasdemo.js
+++ Examples/Canvas/js/canvasdemo.js
@@ -13,3 +13,20 @@ cxt.lineWidth = 2;
 cxt.strokeStyle = "#00ff00";
 cxt.stroke();
 cxt.closePath();
+
+cxt.beginPath();
+cxt.moveTo(150, 100);
+cxt.lineTo(150, 200);
+cxt.lineWidth = 4;
+cxt.strokeStyle = "#ff0000";
+cxt.stroke();
+cxt.closePath();
+
+cxt.beginPath();
+cxt.arc(320, 240, 30, 0, Math.PI, false);
+cxt.lineWidth = 5;
+cxt.fillStyle = "#ffff00";
+cxt.strokeStyle = "#0000ff"; //"blue"
+cxt.stroke();
+cxt.fill();
+cxt.closePath();
