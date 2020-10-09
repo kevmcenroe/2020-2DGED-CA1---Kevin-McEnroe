5437bbc21fa42828267acce7189d512a1eaaf22c
niall mcguinness <niall.mcguinness@dkit.ie>
2020-10-09 11:05:54 +0100

    Added style to canvas

--- Examples/Canvas/js/canvasdemo.js
+++ Examples/Canvas/js/canvasdemo.js
@@ -3,30 +3,34 @@
 var cvs = document.getElementById("main_canvas");
 
 //get a handle to the 2D context of the canvas
-var cxt = cvs.getContext("2d");
+var ctx = cvs.getContext("2d");
 
-//draw 1 or more primitive types (e.g. line, arc, rect)
-cxt.beginPath();
-cxt.moveTo(50, 100);
-cxt.lineTo(250, 100);
-cxt.lineWidth = 2;
-cxt.strokeStyle = "#00ff00";
-cxt.stroke();
-cxt.closePath();
 
-cxt.beginPath();
-cxt.moveTo(150, 100);
-cxt.lineTo(150, 200);
-cxt.lineWidth = 4;
-cxt.strokeStyle = "#ff0000";
-cxt.stroke();
-cxt.closePath();
+ctx.beginPath();
+ctx.moveTo(50, 100);
+ctx.lineTo(250, 100);
+ctx.lineWidth = 2;
+ctx.strokeStyle = "#00ff00";
+ctx.stroke();
+ctx.closePath();
 
-cxt.beginPath();
-cxt.arc(320, 240, 30, 0, Math.PI, false);
-cxt.lineWidth = 5;
-cxt.fillStyle = "#ffff00";
-cxt.strokeStyle = "#0000ff"; //"blue"
-cxt.stroke();
-cxt.fill();
-cxt.closePath();
+ctx.beginPath();
+ctx.moveTo(150, 100);
+ctx.lineTo(150, 200);
+ctx.lineWidth = 4;
+ctx.strokeStyle = "#ff0000";
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(320, 240, 30, 0, Math.PI/2, false);
+ctx.lineWidth = 5;
+ctx.fillStyle = "#ffff00";
+ctx.strokeStyle = "#0000ff"; //"blue"
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
+
+
+
+
