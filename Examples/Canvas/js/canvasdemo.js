2ecaaa8fc1a7d6dd7c5ef3008c9833aaf4d0fb5b
niall mcguinness <niall.mcguinness@dkit.ie>
2020-10-09 12:00:56 +0100

    Added watchmen smile

--- Examples/Canvas/js/canvasdemo.js
+++ Examples/Canvas/js/canvasdemo.js
@@ -5,31 +5,57 @@ var cvs = document.getElementById("main_canvas");
 //get a handle to the 2D context of the canvas
 var ctx = cvs.getContext("2d");
 
+/******************************************************/
 ctx.beginPath();
 //face
 ctx.arc(320, 240, 80, 0, Math.PI * 2);
-
 //left eye
 ctx.moveTo(290, 200);
 ctx.arc(280, 200, 10, 0, Math.PI * 2);
-
 //right eye
 ctx.moveTo(370, 200);
 ctx.arc(360, 200, 10, 0, Math.PI * 2);
-
 //smile
 ctx.moveTo(380, 240);
 ctx.arc(320, 240, 60, 0, Math.PI);
 ctx.stroke();
 ctx.closePath();
+/******************************************************/
 
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.strokeStyle = "black";
+ctx.fillStyle = "yellow";
+ctx.arc(500, 240, 40, 0, Math.PI * 2);
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
 
+//left eye
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.fillStyle = "white";
+ctx.arc(480, 230, 5, 0, Math.PI * 2);
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
 
+//right eye
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.fillStyle = "white";
+ctx.arc(520, 230, 5, 0, Math.PI * 2);
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
 
-
-
-
-
+//line smile
+ctx.beginPath();
+ctx.lineWidth = 1;
+ctx.strokeStyle = "black";
+ctx.arc(500, 245, 25, 0, Math.PI);
+ctx.stroke();
+ctx.closePath();
 
 
 
