ba6617558edbc69be89b7eacb8376a75fb10efff
niall mcguinness <niall.mcguinness@dkit.ie>
2020-10-22 15:35:38 +0100

    Added vertical extent CD

--- Games/Pong/js/pong.js
+++ Games/Pong/js/pong.js
@@ -2,13 +2,13 @@ window.addEventListener("load", loadGame);
 
 var keysDown = {}; //new Array();
 
-window.addEventListener("keydown", function(evt){
-  //  console.log(evt.key);
+window.addEventListener("keydown", function (evt) {
+    //  console.log(evt.key);
     keysDown[evt.key] = true;
 });
 
-window.addEventListener("keyup", function(evt){
-   // console.log(evt.key);
+window.addEventListener("keyup", function (evt) {
+    // console.log(evt.key);
     delete keysDown[evt.key];
 });
 
@@ -21,7 +21,8 @@ var ctx = cvs.getContext("2d");
 //game variables
 var pongBall, paddleLeft, paddleRight;
 var ballSpeed = 5;
-var hitColorA = "yellow", hitColorB = "green";
+var hitColorA = "yellow",
+    hitColorB = "green";
 var hitColor = hitColorA;
 var paddleMoveSpeed = 15;
 
@@ -29,17 +30,16 @@ let height = 80;
 let width = 10;
 let margin = 25;
 
-function demos(){
+function demos() {
     /** DEMO **/
-    try{
+    try {
         var x = GDUtilities.getRandomInRangeExcl(1, 10, [2, 3, 4]);
-    }
-    catch(err){
+    } catch (err) {
         console.log(err);
     }
 }
 
-function loadGame(){
+function loadGame() {
 
     //demos();
 
@@ -50,15 +50,16 @@ function loadGame(){
     window.requestAnimationFrame(animate);
 }
 
-function initializeControls(){
-    paddleLeft = new Rect(new Vector2(margin, (cvs.clientHeight - height)/2), 
-    new Vector2(width, height));
+function initializeControls() {
+    paddleLeft = new Rect(new Vector2(margin, (cvs.clientHeight - height) / 2),
+        new Vector2(width, height));
 
-    paddleRight = new Rect(new Vector2(cvs.clientWidth - margin - width, 
-            (cvs.clientHeight - height)/2), 
-    new Vector2(width, height));
+    paddleRight = new Rect(new Vector2(cvs.clientWidth - margin - width,
+            (cvs.clientHeight - height) / 2),
+        new Vector2(width, height));
 }
-function initializeBall(){
+
+function initializeBall() {
     pongBall = new Arc(320, 240, 20, 0, Math.PI * 2);
     var randomX = GDUtilities.getRandomInRangeExcl(-2, 2, [0]);
     var randomY = GDUtilities.getRandomInRange(-2, 2);
@@ -66,25 +67,25 @@ function initializeBall(){
     ballVector.normalize();
 }
 
-function initializeGame(){
+function initializeGame() {
     initializeControls();
     initializeBall();
 }
 
-function animate(){
+function animate() {
     update();
-    draw();  
+    draw();
     window.requestAnimationFrame(animate);
 }
 
-function update(){
+function update() {
     checkCollisions();
     updateBall();
     updatePaddles();
     updateScore();
 }
 
-function checkCollisions(){
+function checkCollisions() {
 
     //paddle collision with world 
     checkBallToWorldCollision();
@@ -94,65 +95,79 @@ function checkCollisions(){
 /**
  * World extends collision with ball
  */
-function checkBallToWorldCollision(){
-    if(this.pongBall.x >= cvs.clientWidth - this.pongBall.radius){
+function checkBallToWorldCollision() {
+    if (this.pongBall.x >= cvs.clientWidth - this.pongBall.radius) {
         this.ballVector.x *= -1;
         hitColor = hitColorA;
-    }
-    else if(this.pongBall.x <= this.pongBall.radius){
+    } else if (this.pongBall.x <= this.pongBall.radius) {
         this.ballVector.x *= -1;
         hitColor = hitColorB;
     }
 
-    if(this.pongBall.y >= cvs.clientHeight - this.pongBall.radius)
+    if (this.pongBall.y >= cvs.clientHeight - this.pongBall.radius)
         this.ballVector.y *= -1;
-    else if(this.pongBall.y <= this.pongBall.radius)
+    else if (this.pongBall.y <= this.pongBall.radius)
         this.ballVector.y *= -1;
 }
 /**
  * Paddle collision with ball
  */
-function checkBallToPaddleCollision(){
-    //to do...
+function checkBallToPaddleCollision() {
+    //check for ball CD/CR against the left paddle
+    //remember that CD/CR is projected/predictive
+
+    //if we detect a CD then we reverse the ballVector.x
 }
 
 
-function updateBall(){
+function updateBall() {
     this.pongBall.x += ballSpeed * ballVector.x;
     this.pongBall.y += ballSpeed * ballVector.y;
 }
 
-function updatePaddles(){
-    for(var key in this.keysDown){
-        if(key == "w"){
-            this.paddleLeft.move(0, -paddleMoveSpeed);
-        }
-        else if(key == "s"){
-            this.paddleLeft.move(0, paddleMoveSpeed);
+function updatePaddles() {
+    var paddleLeftMove = 0,
+        paddleRightMove = 0;
+    for (var key in this.keysDown) {
+        if (key == "w") {
+            paddleLeftMove = -paddleMoveSpeed;
+        } else if (key == "s") {
+            paddleLeftMove = paddleMoveSpeed;
         }
 
         //right paddle
-        if(key == "o"){
-            this.paddleRight.move(0, -paddleMoveSpeed);
+        if (key == "o") {
+            paddleRightMove = -paddleMoveSpeed;
+        } else if (key == "l") {
+            paddleRightMove = paddleMoveSpeed;
         }
-        else if(key == "l"){
-            this.paddleRight.move(0, paddleMoveSpeed);
+
+        //vertical bounds check on left paddle
+        if (paddleLeftMove + this.paddleLeft.position.y > 20 &&
+            paddleLeftMove + this.paddleLeft.position.y < 460 - this.paddleLeft.dimension.y) {
+            this.paddleLeft.move(0, paddleLeftMove);
+        }
+
+        //vertical bounds check on left paddle
+        if (paddleRightMove + this.paddleRight.position.y > 20 &&
+            paddleRightMove + this.paddleRight.position.y < 460 - this.paddleRight.dimension.y) {
+            this.paddleRight.move(0, paddleRightMove);
         }
     }
 }
 
-function updateScore(){
+function updateScore() {
 
 }
 
-function draw(){
+function draw() {
     clearCanvas("rgb(255, 255, 241)");
     pongBall.draw(ctx, 2, "red", hitColor, false);
     paddleLeft.draw(ctx, 2, "blue");
     paddleRight.draw(ctx, 2, "orange");
 }
 
-function clearCanvas(color){
+function clearCanvas(color) {
     ctx.save();
     ctx.fillStyle = color;
     ctx.fillRect(0, 0, cvs.clientWidth, cvs.clientHeight);
