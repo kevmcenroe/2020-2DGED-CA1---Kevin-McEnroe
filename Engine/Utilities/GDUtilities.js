29c626c6ad2b8b7b6ed0e78e2344cad572f6666c
niall mcguinness <niall.mcguinness@dkit.ie>
2020-10-22 15:04:55 +0100

    Added paddle right movement

--- Engine/Utilities/GDUtilities.js
+++ Engine/Utilities/GDUtilities.js
@@ -1,7 +1,19 @@
 //add a new class with static methods to provide useful functionality for use in our engine
 
-class GDUtilities{
+class GDUtilities {
 
+    static getRandomInRange(lo, hi) {
+
+        //failure tests
+        if (lo == undefined || hi == undefined)
+            //    || typeof(exclValues[0]) == "number")
+            throw "One or more parameters is undefined";
+
+        if (lo > hi)
+            throw "Lo value must be less than hi!";
+
+        return Math.round(((hi - lo) * Math.random()) + lo);
+    }
     /**
      * Method returns a random number in the range lo-hi excluding any numbers listed on the exclValues array
      *
@@ -12,28 +24,31 @@ class GDUtilities{
      * @returns
      * @memberof GDUtilities
      */
-    static getRandomInRangeExcl(lo, hi, exclValues){
-        
+    static getRandomInRangeExcl(lo, hi, exclValues) {
+
         //failure tests
-        if(lo == undefined || hi == undefined 
-            || exclValues == undefined || exclValues == null)
-                //    || typeof(exclValues[0]) == "number")
+        if (lo == undefined || hi == undefined ||
+            exclValues == undefined || exclValues == null)
+            //    || typeof(exclValues[0]) == "number")
             throw "One or more parameters is undefined";
 
-        if(lo > hi)
+        if (exclValues.length == 0)
+            return Math.round(((hi - lo) * Math.random()) + lo);
+
+        if (lo > hi)
             throw "Lo value cannot be greater than value!";
 
         var numArray = new Array();
         var bCollision = false;
 
-        for(let i = lo; i <= hi; i++){
-            for(let j = 0; j < exclValues.length; j++){
-                if(i == exclValues[j]){
+        for (let i = lo; i <= hi; i++) {
+            for (let j = 0; j < exclValues.length; j++) {
+                if (i == exclValues[j]) {
                     bCollision = true;
                     break;
                 }
             }
-            if(!bCollision)
+            if (!bCollision)
                 numArray.push(i);
             bCollision = false;
         }
@@ -41,7 +56,7 @@ class GDUtilities{
         //now I have an array with values lo->hi and excluding exclValues and I shuffle
         GDUtilities.shuffle(numArray);
 
-        var randPos = Math.round(Math.random() * numArray.length-1);
+        var randPos = Math.round(Math.random() * numArray.length - 1);
         //return the first shuffled value
         return numArray[randPos];
     }
@@ -57,23 +72,22 @@ class GDUtilities{
      * @author Geeksforgeeks
      * @since October 2020
      */
-    static shuffle(array){
-        for (var i = array.length - 1; i > 0; i--) {  
+    static shuffle(array) {
+        for (var i = array.length - 1; i > 0; i--) {
             // Generate random number  
-            var j = Math.floor(Math.random() * (i + 1));    
-            var temp = array[i]; 
-            array[i] = array[j]; 
-            array[j] = temp; 
-        } 
-        return array; 
-     } 
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
+    }
 }
 
-class GDMath{
-
-}
-
-class GDString{
+class GDMath {
 
 }
 
+class GDString {
+
+}
--- Games/Pong/js/pong.js
+++ Games/Pong/js/pong.js
@@ -19,7 +19,7 @@ var cvs = document.getElementById("main_canvas");
 var ctx = cvs.getContext("2d");
 
 //game variables
-var pongBall, pongPaddleL, pongPaddleR;
+var pongBall, paddleLeft, paddleRight;
 var ballSpeed = 5;
 var hitColorA = "yellow", hitColorB = "green";
 var hitColor = hitColorA;
@@ -27,7 +27,7 @@ var paddleMoveSpeed = 15;
 
 let height = 80;
 let width = 10;
-let margin = 5;
+let margin = 25;
 
 function demos(){
     /** DEMO **/
@@ -51,16 +51,18 @@ function loadGame(){
 }
 
 function initializeControls(){
-    pongPaddleL = new Rect(new Vector2(margin, (cvs.clientHeight - height)/2), 
+    paddleLeft = new Rect(new Vector2(margin, (cvs.clientHeight - height)/2), 
     new Vector2(width, height));
 
-    pongPaddleR = new Rect(new Vector2(cvs.clientWidth - margin - width, 
+    paddleRight = new Rect(new Vector2(cvs.clientWidth - margin - width, 
             (cvs.clientHeight - height)/2), 
     new Vector2(width, height));
 }
 function initializeBall(){
     pongBall = new Arc(320, 240, 20, 0, Math.PI * 2);
-    ballVector = new Vector2(Math.floor(Math.random() * 10 - 5), Math.floor(Math.random() * 10 - 5));
+    var randomX = GDUtilities.getRandomInRangeExcl(-2, 2, [0]);
+    var randomY = GDUtilities.getRandomInRange(-2, 2);
+    ballVector = new Vector2(randomX, randomY);
     ballVector.normalize();
 }
 
@@ -76,9 +78,6 @@ function animate(){
 }
 
 function update(){
-    
-  //  console.log('keysDown :>> ', keysDown);
-
     checkCollisions();
     updateBall();
     updatePaddles();
@@ -87,25 +86,36 @@ function update(){
 
 function checkCollisions(){
 
-    //ball
-    if(this.pongBall.x >= cvs.clientWidth - 20){
+    //paddle collision with world 
+    checkBallToWorldCollision();
+    checkBallToPaddleCollision();
+}
+
+/**
+ * World extends collision with ball
+ */
+function checkBallToWorldCollision(){
+    if(this.pongBall.x >= cvs.clientWidth - this.pongBall.radius){
         this.ballVector.x *= -1;
         hitColor = hitColorA;
     }
-    else if(this.pongBall.x <= 20){
+    else if(this.pongBall.x <= this.pongBall.radius){
         this.ballVector.x *= -1;
         hitColor = hitColorB;
     }
 
-    if(this.pongBall.y >= cvs.clientHeight - 20)
+    if(this.pongBall.y >= cvs.clientHeight - this.pongBall.radius)
         this.ballVector.y *= -1;
-    else if(this.pongBall.y <= 20)
+    else if(this.pongBall.y <= this.pongBall.radius)
         this.ballVector.y *= -1;
-
-    //paddle collision with world 
-
-    //paddle collision with ball
 }
+/**
+ * Paddle collision with ball
+ */
+function checkBallToPaddleCollision(){
+    //to do...
+}
+
 
 function updateBall(){
     this.pongBall.x += ballSpeed * ballVector.x;
@@ -115,13 +125,19 @@ function updateBall(){
 function updatePaddles(){
     for(var key in this.keysDown){
         if(key == "w"){
-            this.pongPaddleL.move(0, -paddleMoveSpeed);
+            this.paddleLeft.move(0, -paddleMoveSpeed);
         }
         else if(key == "s"){
-            this.pongPaddleL.move(0, paddleMoveSpeed);
+            this.paddleLeft.move(0, paddleMoveSpeed);
         }
 
         //right paddle
+        if(key == "o"){
+            this.paddleRight.move(0, -paddleMoveSpeed);
+        }
+        else if(key == "l"){
+            this.paddleRight.move(0, paddleMoveSpeed);
+        }
     }
 }
 
@@ -129,14 +145,11 @@ function updateScore(){
 
 }
 
-
-
 function draw(){
     clearCanvas("rgb(255, 255, 241)");
-
     pongBall.draw(ctx, 2, "red", hitColor, false);
-    pongPaddleL.draw(ctx, 2, "blue");
-    pongPaddleR.draw(ctx, 2, "orange");
+    paddleLeft.draw(ctx, 2, "blue");
+    paddleRight.draw(ctx, 2, "orange");
 }
 
 function clearCanvas(color){
