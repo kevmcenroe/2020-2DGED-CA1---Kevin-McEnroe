ddfa211d87cb8d6efe8c018e277c97661ad8a78a
niall mcguinness <niall.mcguinness@dkit.ie>
2020-11-20 11:19:47 +0000

    Added exercise description

--- Exercises/InheritanceAndClone/js/exercises.js
+++ Exercises/InheritanceAndClone/js/exercises.js
@@ -2,6 +2,25 @@ document.writeln("Open the Inspect/Console pane in Chrome to see the output...")
 
 runExercises();
 
+/*
+    1. Implement a Player
+        - name, position(x,y), texture(name)
+        - inventory(weapons) - array
+    2. Implment a Weapon
+        - name, texture(name)
+        - clipSize (2MW)
+        - damageRate (1200)
+        - clipCount (10MW)
+    3. Implement a Sprite
+        - name
+        - texture(name)
+    4. Populate the Player with 3-4 Weapons
+    5. Clone a Weapon test using Equals (w1 === w2?)
+    6. Clone a Player test using Equals (p1 === p2)
+ */
+
+
+
 function runExercises()
 {
  
