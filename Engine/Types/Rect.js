811c9e958aaf5ca9da3d2ec9efafcbf6665c8e3f
niall mcguinness <niall.mcguinness@dkit.ie>
2020-11-22 17:29:50 +0000

    Added comments to Rect and Vector2

--- Engine/Types/Rect.js
+++ Engine/Types/Rect.js
@@ -1,5 +1,7 @@
 /**
  * Represents a 2D rectangle (x, y, width, height) which is typically used for collision detection/collision response.
+ * For example, a drawn character (i.e. a Sprite) will use a Rect object to define a collision surface for the character.
+ * 
  * @author niall mcguinness
  * @version 1.0
  * @class Rect
@@ -9,6 +11,17 @@ class Rect {
   //#endregion
 
   //#region Properties
+
+  /**
+   *
+   * Returns an instance of a Rect object with w.h of 1x1.
+   * Usage:
+   *      let r = Rect.Zero;
+   * 
+   * @readonly
+   * @static
+   * @memberof Rect
+   */
   static get Zero() {
     return new Rect(0, 0, 1, 1);
   }
@@ -25,8 +38,14 @@ class Rect {
     return this.height;
   }
 
+  /**
+   * Returns the coordinates of the centre point of a Rect object with top left (x,y) and dimensions (w,h) as a Vector2.
+   *
+   * @readonly
+   * @memberof Rect
+   */
   get Center() {
-    return new Vector2(this.x + this.width / 2, this.y + this.height / 2);
+    return new Vector2(Math.round(this.x + this.width / 2), Math.round(this.y + this.height / 2));
   }
 
   set X(value) {
@@ -44,17 +63,33 @@ class Rect {
   //#endregion
 
   //#region Constructors and Core methods
+
+  /**
+   * Constructs a Rect object (x,y,w,h) 
+   * @param {Number} x X-ordinate of the top-left corner of the Rect object
+   * @param {Number} y Y-ordinate of the top-left corner of the Rect object
+   * @param {Number} width Width in pixels of the Rect object  
+   * @param {Number} height Height in pixels of the Rect object  
+   */
   constructor(x, y, width, height) {
     this.x = x;
     this.y = y;
-    this.originalX = x;
-    this.originalY = y;
     this.width = width;
     this.height = height;
+
+    //if we ever change the Rect object (e.g. x, y, w, h) then we may want to reset it so we store original values
+    this.originalX = x;
+    this.originalY = y;
     this.originalWidth = width;
     this.originalHeight = height;
   }
 
+
+  /**
+   * Resets the Rect object to its original values (x,y,w,h)
+   *
+   * @memberof Rect
+   */
   Reset() {
     this.x = this.originalX;
     this.y = this.originalY;
@@ -63,11 +98,20 @@ class Rect {
     this.height = this.originalHeight;
   }
 
-  Move(vector) {
-    this.x += vector.x;
-    this.y += vector.y;
+  /**
+   * Moves a Rect object (i.e. x,y) by a delta Vector2
+   * @param {Vector2} delta A Vector2 added to the (x,y) values of the Rect to move it. 
+   */
+  Move(delta) {
+    this.x += delta.x;
+    this.y += delta.y;
   }
 
+  /**
+   * Moves a Rect object (i.e. x,y) by a x and y values
+   * @param {Number} x A integer value to add to the X-ordinate
+   * @param {Number} y A integer value to add to the Y-ordinate
+   */
   Move(x, y) {
     this.x += x;
     this.y += y;
@@ -75,54 +119,193 @@ class Rect {
   //#endregion
 
   //#region Collision Detection
-  Contains(otherRect) {
-    let enclosingRect = this.GetEnclosingRect(otherRect);
+
+  /**
+   * Called when we want to determine if "a" Rect object contains (has completely inside) the "b" Rect object
+   * 
+   * Usage:
+   *  var a = new Rect(0, 0, 100, 100);
+   *  var b = new Rect(5, 5, 20, 20)
+   *  var bContains = a.Contains(b);
+   * 
+   * @param {Rect} rect A Rect object which we will test if it is inside the Rect object on the LHS of the dot operator
+   * @returns {Boolean} True if b inside a, otherwise false
+   */
+  Contains(rect) {
+    let enclosingRect = this.GetEnclosingRect(rect);
     return (
-      enclosingRect.Width == Math.max(this.width, otherRect.Width) &&
-      enclosingRect.Height == Math.max(this.height, otherRect.Height)
+      enclosingRect.Width == Math.max(this.width, rect.Width) &&
+      enclosingRect.Height == Math.max(this.height, rect.Height)
     );
   }
 
-  Intersects(otherRect) {
-    let enclosingRect = this.GetEnclosingRect(otherRect);
+   /**
+   * Called when we want to determine if "a" Rect object intersects (overlaps) the "b" Rect object
+   * 
+   * Usage:
+   *  var a = new Rect(0, 0, 100, 100);
+   *  var b = new Rect(0, 50, 100, 100)
+   *  var bContains = a.Intersects(b);
+   * 
+   * @param {Rect} rect A Rect object which we will test if it intersect with the Rect object on the LHS of the dot operator 
+   * @returns {Boolean} True if b intersects a (or vice versa), otherwise false
+   */
+  Intersects(rect) {
+    let enclosingRect = this.GetEnclosingRect(rect);
     return (
-      enclosingRect.Width <= this.width + otherRect.Width &&
-      enclosingRect.Height <= this.height + otherRect.Height
+      enclosingRect.Width <= this.width + rect.Width &&
+      enclosingRect.Height <= this.height + rect.Height
     );
   }
 
-  GetEnclosingRect(otherRect) {
+  /**
+   * Called when we want to return a Rect object that contains both "a" and "b" Rect object.
+   * 
+   *  Usage:
+   *  var a = new Rect(0, 0, 100, 100);
+   *  var b = new Rect(100, 0, 100, 100)
+   *  var enclosing = a.GetEnclosingRect(b); //enclosing = (0, 0, 200, 100)
+   * 
+   * @param {Rect} rect A Rect object
+   * @returns {Rect} A Rect object that encloses both "a" and "b" 
+   */
+  GetEnclosingRect(rect) {
     if (
-      otherRect == null ||
-      otherRect == undefined ||
-      !otherRect instanceof Rect
+      rect == null ||
+      rect == undefined ||
+      !rect instanceof Rect
     )
       throw (
         "Error: One or more objects is null, undefined, or not type " +
         this.constructor.name
       );
 
-    let minX = Math.min(this.x, otherRect.x);
-    let minY = Math.min(this.y, otherRect.y);
+    let minX = Math.min(this.x, rect.x);
+    let minY = Math.min(this.y, rect.y);
 
     let width =
-      Math.max(this.x + this.width, otherRect.x + otherRect.width) - minX;
+      Math.max(this.x + this.width, rect.x + rect.width) - minX;
     let height =
-      Math.max(this.y + this.height, otherRect.y + otherRect.height) - minY;
+      Math.max(this.y + this.height, rect.y + rect.height) - minY;
 
     return new Rect(minX, minY, width, height);
   }
   //#endregion
 
+  //#region Static Methods
+
+    /**
+   * Called when we want to determine if "a" Rect object contains (completely contains) the "b" Rect object
+   * Usage:
+   *  var a = new Rect(0, 0, 100, 100);
+   *  var b = new Rect(5, 5, 20, 20)
+   *  var bContains = Rect.Contains(a, b);
+   * 
+   * @param {Rect} a A Rect object 
+   * @param {Rect} b A Rect object 
+   * @returns {Boolean} True if b inside a, otherwise false
+   */
+  static Contains(a, b) {
+    if (a == null || a == undefined || !a instanceof Rect)
+      throw (
+        "Error: One or more objects is null, undefined, or not type " +
+        a.constructor.name
+      );
+
+    return a.Contains(b);
+  }
+
+    /**
+   * Called when we want to determine if "a" Rect object intersect (overlaps) the "b" Rect object
+   * 
+   * Usage:
+   *  var outerRect = new Rect(0, 0, 100, 100);
+   *  var innerRect = new Rect(0, 50, 100, 100)
+   *  var bContains = Rect.Intersects(a, b);
+   * 
+   * @param {Rect} a A Rect object 
+   * @param {Rect} b A Rect object 
+   * @returns {Boolean} True if b intersects a (or vice versa), otherwise false
+   */
+  static Intersects(a, b) {
+    if (a == null || a == undefined || !a instanceof Rect)
+      throw (
+        "Error: One or more objects is null, undefined, or not type " +
+        a.constructor.name
+      );
+
+    return a.Intersects(b);
+  }
+
+    /**
+   * Called when we want to return a Rect object that contains both "a" and "b" Rect object.
+   * 
+   *  Usage:
+   *  var a = new Rect(0, 0, 100, 100);
+   *  var b = new Rect(100, 0, 100, 100)
+   *  var enclosing = Rect.GetEnclosingRect(a,b); //enclosing = (0, 0, 200, 100)
+   * 
+   * @param {Rect} a A Rect object
+   * @param {Rect} b A Rect object
+   * @returns {Rect} A Rect object that encloses both "a" and "b" 
+   */
+  static GetEnclosingRect(a, b) {
+    if (a == null || a == undefined || !a instanceof Rect)
+      throw (
+        "Error: One or more objects is null, undefined, or not type " +
+        a.constructor.name
+      );
+
+    return a.GetEnclosingRect(b);
+  }
+
+  /**
+   *  Moves a Rect object by a delta Vector2 and returns a NEW Rect object
+   * 
+   * Usage:
+   * var original = new Rect(0,0,100,100);
+   * var moved = Rect.Move(original, new Vector2(25, 25)); //moved = (25, 25, 125, 125)
+   * 
+   * @param {Rect} rect A Rect object
+   * @param {Vector2} vector A Vector2 object
+   * @returns {Vector2} A transformed distinct (i.e. a deep copy) Rect object
+   */
+  static Move(rect, delta) {
+    let clone = rect.Clone();
+    clone.Move(delta);
+    return clone;
+  }
+
+  /**
+   * Rounds the (x,y,w,h) values of a Rect object and returns a NEW Rect object
+   * 
+   * Usage:
+   * var original = new Rect(1.55,1.91,100.1,100.9);
+   * var rounded = Rect.Round(original, 1); //rounded = (1.6, 1.9, 100.0, 101.0)
+   * 
+   * @param {Rect} rect A Rect object typically containing floating point (x,y,w,h) values
+   * @param {Number} precision An integer rounding precision value
+   * @returns {Vector2} A rounded distinct (i.e. a deep copy) Rect object
+   */
+  static Round(rect, precision) {
+    return new Rect(
+      GDMath.ToFixed(rect.x, precision, 10),
+      GDMath.ToFixed(rect.y, precision, 10),
+      GDMath.ToFixed(rect.width, precision, 10),
+      GDMath.ToFixed(rect.height, precision, 10)
+    );
+  }
+  //#endregion
+
   //#region Equals, Clone, ToString
-  Equals(otherRect) {
+  Equals(rect) {
     //if we get here then we have two valid (i.e. non-null, defined, correct type) and distinct (i.e. separate RAM) objects that we need to test
     return (
-      GDUtility.IsSameTypeAsTarget(this, otherRect) &&
-      this.x === otherRect.X &&
-      this.y === otherRect.Y &&
-      this.width === otherRect.Width &&
-      this.height === otherRect.Height
+      GDUtility.IsSameTypeAsTarget(this, rect) &&
+      this.x === rect.X &&
+      this.y === rect.Y &&
+      this.width === rect.Width &&
+      this.height === rect.Height
     );
   }
 
@@ -141,50 +324,4 @@ class Rect {
   }
   //#endregion
 
-  //#region Static Methods
-  static Contains(rect1, rect2) {
-    if (rect1 == null || rect1 == undefined || !rect1 instanceof Rect)
-      throw (
-        "Error: One or more objects is null, undefined, or not type " +
-        rect1.constructor.name
-      );
-
-    return rect1.Contains(rect2);
-  }
-
-  static Intersects(rect1, rect2) {
-    if (rect1 == null || rect1 == undefined || !rect1 instanceof Rect)
-      throw (
-        "Error: One or more objects is null, undefined, or not type " +
-        rect1.constructor.name
-      );
-
-    return rect1.Intersects(rect2);
-  }
-
-  static GetEnclosingRect(rect1, rect2) {
-    if (rect1 == null || rect1 == undefined || !rect1 instanceof Rect)
-      throw (
-        "Error: One or more objects is null, undefined, or not type " +
-        rect1.constructor.name
-      );
-
-    return rect1.GetEnclosingRect(rect2);
-  }
-
-  static Move(rect, vector) {
-    let clone = rect.Clone();
-    clone.Move(vector);
-    return clone;
-  }
-
-  static Round(rect, precision) {
-    return new Rect(
-      GDMath.ToFixed(rect.x, precision, 10),
-      GDMath.ToFixed(rect.y, precision, 10),
-      GDMath.ToFixed(rect.width, precision, 10),
-      GDMath.ToFixed(rect.height, precision, 10)
-    );
-  }
-  //#endregion
 }
--- Engine/Types/Vector2.js
+++ Engine/Types/Vector2.js
@@ -1,5 +1,5 @@
 /**
- * Represents a 2D vector within the game engine.
+ * Represents a 2D vector within the game engine. Used in Transform2D to store translation, scale and dimensions of a Sprite.
  * @author niall mcguinness
  * @version 1.0
  * @class Vector2
@@ -10,15 +10,59 @@ class Vector2 {
   //#endregion
 
   //#region Properties
+
+  /**
+   *
+   * Returns an instance of a Vector2 object with x,y of (0,0).
+   * Usage:
+   *      let v = Vector2.Zero;
+   *
+   * @readonly
+   * @static
+   * @memberof Vector2
+   */
   static get Zero() {
     return new Vector2(0, 0);
   }
+
+  /**
+   *
+   * Returns an instance of a Vector2 object with x,y of (1,1).
+   * Usage:
+   *      let v = Vector2.One;
+   *
+   * @readonly
+   * @static
+   * @memberof Vector2
+   */
   static get One() {
     return new Vector2(1, 1);
   }
+
+  /**
+   *
+   * Returns an instance of a Vector2 object with x,y of (1,0).
+   * Usage:
+   *      let v = Vector2.UnitX;
+   *
+   * @readonly
+   * @static
+   * @memberof Vector2
+   */
   static get UnitX() {
     return new Vector2(1, 0);
   }
+
+  /**
+   *
+   * Returns an instance of a Vector2 object with x,y of (0, 1).
+   * Usage:
+   *      let v = Vector2.UnitY;
+   *
+   * @readonly
+   * @static
+   * @memberof Vector2
+   */
   static get UnitY() {
     return new Vector2(0, 1);
   }
@@ -30,57 +74,110 @@ class Vector2 {
   }
   set X(value) {
     this.x = value;
-    //this.isDirty = true;
   }
   set Y(value) {
     this.y = value;
-   // this.isDirty = true;
   }
   //#endregion
 
   //#region Constructors and Core methods
+
+  /**
+   * Returns a Vector2 object
+   * @param {Number} x 
+   * @param {Number} y 
+   */
   constructor(x, y) {
     this.x = x;
     this.y = y;
-   // this.isDirty = true;
   }
 
-  Add(otherVector) {
-    this.x += otherVector.x;
-    this.y += otherVector.y;
+  /**
+   * Modifies the Vector2 by adding the individual (x,y) values of a second Vector2 object to the object
+   * @param {Rect} other A Vector2 object
+   */
+  Add(other) {
+    this.x += other.x;
+    this.y += other.y;
   }
 
-  Subtract(otherVector) {
-    this.x -= otherVector.x;
-    this.y -= otherVector.y;
+  /**
+   * Modifies the Vector2 by subtracting the individual (x,y) values of a second Vector2 object from the object
+   * @param {Rect} other A Vector2 object
+   */
+  Subtract(other) {
+    this.x -= other.x;
+    this.y -= other.y;
   }
 
-  Multiply(otherVector) {
-    this.x *= otherVector.x;
-    this.y *= otherVector.y;
+  /**
+   * Modifies the Vector2 by multiplying the (x,y) values by the individual (x,y) values of a second Vector2 object
+   * @param {Rect} other A Vector2 object
+   */
+  Multiply(other) {
+    this.x *= other.x;
+    this.y *= other.y;
   }
 
+  /**
+   * Modifies the Vector2 by multiplying the (x,y) values by a scalar quantity
+   * @param {Number} s A scalar quantity 
+   */
   MultiplyScalar(s) {
     this.x *= s;
     this.y *= s;
   }
 
-  Divide(otherVector) {
-    //divide by 0!!!
-    this.x /= otherVector.x;
-    this.y /= otherVector.y;
+  /**
+   * Modifies the Vector2 by dividing the (x,y) values by the individual (x,y) values of a second Vector2 object
+   * @param {Number} s A scalar quantity 
+   * @throws Exception if either x or y value of the other vector == 0
+   */
+  Divide(other) {
+    //divide by 0!
+    if(other.x == 0 || other.y == 0)
+      throw "Cannot divide by Zero!";
+
+    this.x /= other.x;
+    this.y /= other.y;
   }
 
+  /**
+   * Modifies the Vector2 by dividing the (x,y) values by a scalar quantity
+   * @param {Number} s A scalar quantity 
+   * @throws Exception if either x or y value of the other vector == 0
+   */
   DivideScalar(s) {
-     //divide by 0!!!
+    //divide by 0!
+    if(s == 0)
+    throw "Cannot divide by Zero!";
+
     this.x /= s;
     this.y /= s;
   }
 
-  Dot(otherVector) {
-    return this.x * otherVector.x + this.y * otherVector.y;
+  /**
+   * Returns the dot product (A.B) of two Vector2 objects, where dot = |A|.|B|.Cos(x) and x is angle between A and B in radians
+   * @param {Vector2} other A Vector2 object
+   * @returns {Number} Dot product 
+   */
+  Dot(other) {
+    return this.x * other.x + this.y * other.y;
   }
 
+   /**
+   * Returns the angle in radians between to Vector2 objects which is calculated using the Dot() method
+   * @param {Vector2} other A Vector2 object
+   * @returns {Number} Angle in radians 
+   */
+  AngleInRadiansBetween(other) {
+    return Math.acos(this.Dot(other) / (this.Length() * other.Length()));
+  }
+
+   /**
+   * Returns the length of a Vector2 object
+   * @returns {Number} Length of the Vector2
+   */
   Length() {
     if (this.isDirty) {
       this.length = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2)); //PYTHAGORAS
@@ -89,80 +186,70 @@ class Vector2 {
     return this.length;
   }
 
-  AngleInRadiansBetween(otherVector) {
-    return Math.acos(
-      this.Dot(otherVector) / (this.Length() * otherVector.Length())
-    );
-  }
-
-  Distance(otherVector) {
+   /**
+   * Returns the Euclidean (i.e. straight line) distance between two points in 2D space
+   * @param {Vector2} other A Vector2 object
+   * @returns {Number} Distance between the two vectors
+   */
+  Distance(other) {
     return Math.sqrt(
-      Math.pow(this.x - otherVector.x, 2) + Math.pow(this.y - otherVector.y, 2)
+      Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2)
     );
   }
 
+   /**
+   * Modifies the Vector2 object where the (x,y) values have had the Math.abs() method applied.
+   */
   Abs() {
     this.x = Math.abs(this.x);
     this.y = Math.abs(this.y);
   }
 
+   /**
+   * Modifies the Vector2 object where the (x,y) values have had the Math.abs() method applied.
+   */
   Round(precision) {
     this.x = GDMath.ToFixed(this.x, precision, 10);
     this.y = GDMath.ToFixed(this.y, precision, 10);
   }
 
+  /**
+   * Modifies the Vector2 object by normalising its length (i.e. == 1)
+   * @throws Throws an exception if length == 0
+   */
   Normalize() {
     var len = this.Length();
 
-    if (len == 0) 
+    if (len == 0)
       throw "Error: Divide by zero error on Normalize()! Is the vector non-zero?";
 
-      this.x /= len;
-      this.y /= len;
-  }
-  //#endregion
-
-  //#region Equals, Clone, ToString
-  Equals(otherVector) {
-    return (
-      GDUtility.IsSameTypeAsTarget(this, otherVector) &&
-      this.x === otherVector.x &&
-      this.y === otherVector.y
-    );
-  }
-
-  //deep-copy => (x,y) => 
-  Clone() {
-    return new Vector2(this.x, this.y);
-  }
-
-  ToString() {
-    return "[" + this.x + "," + this.y + "]";
+    this.x /= len;
+    this.y /= len;
   }
   //#endregion
 
   //#region Static Methods
-  static Add(vector1, vector2) {
-    return new Vector2(vector1.x + vector2.x, vector1.y + vector2.y);
+  static Add(vector1, other) {
+    return new Vector2(vector1.x + other.x, vector1.y + other.y);
   }
 
   //let vC = Vector2.Substract(vA, vB);
-  static Subtract(vector1, vector2) {
-    return new Vector2(vector1.x - vector2.x, vector1.y - vector2.y);
+  static Subtract(vector1, other) {
+    return new Vector2(vector1.x - other.x, vector1.y - other.y);
   }
 
-  static Multiply(vector1, vector2) {
-    return new Vector2(vector1.x * vector2.x, vector1.y * vector2.y);
+  static Multiply(vector1, other) {
+    return new Vector2(vector1.x * other.x, vector1.y * other.y);
   }
 
   static MultiplyScalar(vector, scalar) {
     return new Vector2(vector.x * scalar, vector.y * scalar);
   }
 
-  static Divide(vector1, vector2) {
-    if (vector2.x == 0 || vector2.y == 0) throw "Error: Cannot divide by zero!";
+  static Divide(vector1, other) {
+    if (other.x == 0 || other.y == 0) throw "Error: Cannot divide by zero!";
 
-    return new Vector2(vector1.x / vector2.x, vector1.y / vector2.y);
+    return new Vector2(vector1.x / other.x, vector1.y / other.y);
   }
 
   static DivideScalar(vector, scalar) {
@@ -178,8 +265,8 @@ class Vector2 {
     return new Vector2(vector.x / len, vector.y / len);
   }
 
-  static Distance(vector1, vector2) {
-    return vector1.Distance(vector2);
+  static Distance(vector1, other) {
+    return vector1.Distance(other);
   }
 
   static Abs(vector) {
@@ -198,6 +285,24 @@ class Vector2 {
       GDMath.ToFixed(vector.y, precision, 10)
     );
   }
+  //#endregion
 
+  //#region Equals, Clone, ToString
+  Equals(other) {
+    return (
+      GDUtility.IsSameTypeAsTarget(this, other) &&
+      this.x === other.x &&
+      this.y === other.y
+    );
+  }
+
+  //deep-copy => (x,y) =>
+  Clone() {
+    return new Vector2(this.x, this.y);
+  }
+
+  ToString() {
+    return "[" + this.x + "," + this.y + "]";
+  }
   //#endregion
 }
