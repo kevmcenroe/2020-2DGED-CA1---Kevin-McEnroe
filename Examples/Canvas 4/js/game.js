3f1a9e342491e38add40cb5e5411edf746bd70a2
niall mcguinness <niall.mcguinness@dkit.ie>
2020-12-03 14:55:35 +0000

    Added Canvas 4 demo

--- Examples/Canvas 4/js/game.js
+++ Examples/Canvas 4/js/game.js
@@ -1,3 +1,5 @@
+'use strict'
+
 window.addEventListener("load", Start);
 
 /********************************* Game Engine Core Variables & Functions (Do Not Change in Your Game) *********************************/
@@ -8,53 +10,30 @@ var cvs = document.getElementById("main_canvas");
 //get a handle to the 2D context of the canvas
 var ctx = cvs.getContext("2d");
 
-var movingRectXPos = 20, movingRectYPos = 20;
-var moveIncrement = 4;
-
 //start the loop
 function Start() {
   //start Update/Draw cycle i.e. start the game
   window.requestAnimationFrame(Animate);
 }
 
-//Pascal e.g. GetTime or Camel e.g. getTime
-function Animate(currentTimeInMs) {
+function Animate(now) {
 
   //update all sprites whose state can change over time
-  Update(currentTimeInMs);
+  Update();
 
   //draw all sprite
-  Draw(currentTimeInMs);
+  Draw();
 
   //request the next frame to repeat the update/draw cycle
   window.requestAnimationFrame(Animate);
 }
 
-function Update(currentTimeInMs) {
-
-  UpdateMovingRect(currentTimeInMs);
+function Update() {
+ 
 }
 
-function UpdateMovingRect(currentTimeInMs){
-  movingRectXPos += moveIncrement;
-  //make decisions about state of objects here...
-  if(movingRectXPos > cvs.clientWidth - 150)
-    moveIncrement *= -1;
-  else if(movingRectXPos < 0)
-    moveIncrement *= -1;
-}
-
-
-function Draw(currentTimeInMs) {
-  ClearCanvas("rgb(187, 251, 209)");
-  DrawMovingRect(currentTimeInMs);
-
-}
-
-function DrawMovingRect(currentTimeInMs){
-  ctx.lineWidth = 5;
-  ctx.strokeStyle = "yellow";
-  ctx.strokeRect(movingRectXPos, movingRectYPos, 150, 100);
+function Draw() {
+ 
 }
 
 function ClearCanvas(color) {
